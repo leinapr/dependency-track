9b373c37123fefb9b118bfc3c5e706ee17c810ee
leinapr <162492449+leinapr@users.noreply.github.com>
2024-10-22 02:54:42 +0200

    Update Product.java
    Signed-off-by: leinapr <162492449+leinapr@users.noreply.github.com>

--- src/main/java/org/dependencytrack/model/Product.java
+++ src/main/java/org/dependencytrack/model/Product.java
@@ -1,7 +1,6 @@
 package org.dependencytrack.model;
 
 import javax.persistence.*;
-import java.util.Set;
 
 @Entity
 @Table(name = "PRODUCT")
@@ -17,8 +16,36 @@ public class Product {
     @Column(name = "cpe_code", nullable = false, unique = true)
     private String cpeCode;
 
-    @OneToMany(mappedBy = "produit", cascade = CascadeType.ALL)
-    private Set<CotsOss> cotsOssSet;
+    // Constructors
+    public Product() {}
 
-    // Getters et setters
+    public Product(String name, String cpeCode) {
+        this.name = name;
+        this.cpeCode = cpeCode;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpeCode() {
+        return cpeCode;
+    }
+
+    public void setCpeCode(String cpeCode) {
+        this.cpeCode = cpeCode;
+    }
 }
